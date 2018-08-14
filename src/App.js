3973dbe60ede45003c1e23a4372984e6b60c077b
GanYihuan <583520052@qq.com>
2018-08-14 10:43:19 +0800

    9-1 详情页面布局

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import store from './store'
 import Header from './common/header'
 import Home from './pages/home'
-// import Detail from './pages/detail/loadable.js'
+// import Detail from ' ./pages/detail/loadable.js'
 // import Login from './pages/login'
 // import Write from './pages/write'
 
